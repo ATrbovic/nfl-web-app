e87373ae7783f8f1e5b8da9321498dbb2e509d2c
Alex Trbovic <alextrbovic@outlook.com>
2023-09-18 08:49:52 -0400

    transforming tracking data

--- nfl-web/src/App.js
+++ nfl-web/src/App.js
@@ -1,21 +1,63 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import * as d3 from 'd3';
 import FootballField from './components/footballField';
 
+function transformDataForVisualization(data) {
+  // Group data by frameId
+  const groupedByFrame = d3.group(data, d => d.frameId);
+
+  // Transform data
+  const transformedData = Array.from(groupedByFrame.entries()).map(([frameId, entries]) => {
+      let frameData = {
+          frameId: frameId,
+          players: [],
+          football: null
+      };
+
+      entries.forEach(entry => {
+          if (entry.team !== 'football') {
+              frameData.players.push({
+                  x: +entry.x,
+                  y: +entry.y,
+                  jerseyNumber: entry.jerseyNumber,
+                  team: entry.team,
+                  nflId: entry.nflId
+              });
+          } else {
+              frameData.football = {
+                  x: +entry.x,
+                  y: +entry.y
+              };
+          }
+      });
+
+      return frameData;
+  });
+
+  return transformedData;
+}
+
 function App() {
+    const [playData, setPlayData] = useState(null);
 
-  const data = {}; // Keep in mind to populate this with your actual data or fetch it
+    useEffect(() => {
+        d3.csv('/path/to/playersData.csv').then(data => {
+            // Filter data for a specific game and play
+            const specificPlayData = data.filter(d => d.gameId === "someGameId" && d.playId === "somePlayId");
 
-  return (
-    <div className="App">
-      <header className="site-header">
-        <h1>Welcome to Our Football Visualization</h1>
-      </header>
-      <h2>Football Field</h2>
-      <FootballField data={data} />
-    </div>
-  );
+            // Transform data for visualization
+            const transformedData = transformDataForVisualization(specificPlayData);
+
+            setPlayData(transformedData);
+        });
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Welcome to Our Football Visualization</h1>
+            {playData && <FootballField data={playData} />}
+        </div>
+    );
 }
 
 export default App;
-
