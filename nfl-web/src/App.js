b6dd0d3fd1f6799346b5d94f13985fbf0b51ff86
Alex Trbovic <alextrbovic@outlook.com>
2023-09-18 08:38:03 -0400

    Update to app.js
    Removed the react logo. Imported the football Field component

--- nfl-web/src/App.js
+++ nfl-web/src/App.js
@@ -1,32 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import FootballField from './components/footballField';
 
 function App() {
 
-  const data = {};
+  const data = {}; // Keep in mind to populate this with your actual data or fetch it
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          Hello World
-        </a>
+      <header className="site-header">
+        <h1>Welcome to Our Football Visualization</h1>
       </header>
-      <h1>Football Field</h1>
+      <h2>Football Field</h2>
       <FootballField data={data} />
     </div>
   );
 }
 
 export default App;
+
